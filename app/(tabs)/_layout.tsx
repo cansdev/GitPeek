2c9e2bc64753b446ff7ede9af040d138d0230239
can.sari <can.sari@TUR066P22S.home>
2024-07-08 16:42:59 +0300

    fix: fixed the problem where UserProfile would remain unchanged even after clicking on different profiles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,7 +39,6 @@ export default function TabLayout() {
         name="repoList"
         options={{
           title:"Repo List",
-          tabBarIconStyle: { display: "none" },
           tabBarButton: () => null,
         }}
         />
--- app/(tabs)/userProfile.tsx
+++ app/(tabs)/userProfile.tsx
@@ -1,12 +1,11 @@
-import { View, Text, StyleSheet, ActivityIndicator, Button } from 'react-native';
-import ProfileCard from '../ProfileCard';
-import { Key, useEffect, useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import axios from 'axios';
+import ProfileCard from '../ProfileCard';
 import RepoButton from '../RepoButton';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function Tab({ }: any) {
-
   const [userData, setUserData] = useState({
     id: '',
     login: '',
@@ -14,24 +13,31 @@ export default function Tab({ }: any) {
     following: '',
     avatar_url: ''
   });
-  const user = useLocalSearchParams();
+  const [loading, setLoading] = useState(true); 
 
+  const user = useLocalSearchParams();
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
+        setLoading(true); 
         const response = await axios.get(`https://api.github.com/users/${user.login}`);
         setUserData(response.data);
       } catch (error) {
         console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false); 
       }
     };
+
     fetchUserData();
-  }, []);
+  }, [user.login]);
 
   return (
     <View style={styles.container}>
-      {userData.id ? (
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
         <>
           <ProfileCard
             key={userData.id}
@@ -42,8 +48,6 @@ export default function Tab({ }: any) {
           />
           <RepoButton user={user}/>
         </>
-      ) : (
-        <ActivityIndicator size="large" color="#0000ff" />
       )}
     </View>
   );
